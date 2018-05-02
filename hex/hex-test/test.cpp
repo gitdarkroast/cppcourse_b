d373d57e0ba2ba293f18073fa7c2d01fc37e8d3a
Hanif Ladha <haladha@gmail.com>
2018-05-02 15:15:35 -0700

    Initial ADTs for the HEX game.  Googletest working.

--- hex/hex-test/test.cpp
+++ hex/hex-test/test.cpp
@@ -1,8 +1,93 @@
 #include "pch.h"
 
-#include "..\hex\hex.h"
+#include "../hex/hex.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
+namespace {
+	class BoardTest : public ::testing::Test
+	{
+	};
+
+	TEST(BoardTest, TestDefaultSize) {
+		EXPECT_TRUE(new Board());
+	}
+	TEST(BoardTest, TestSizeProvided) {
+		int size{ 10 };
+		EXPECT_TRUE(new Board(size));
+	}
+	TEST(BoardTest, TestGetNodeValue) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_EQ(0, b.get_node(Point(0, 0)));
+		EXPECT_EQ(size*size, b.get_node(Point(size, 0)));
+		EXPECT_EQ(size, b.get_node(Point(0, size)));
+		EXPECT_EQ(size*size + size, b.get_node(Point(size, size)));
+	}
+
+	TEST(BoardTest, TestIsCorner) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_TRUE(b.isCorner(Point(0, 0)));
+		EXPECT_FALSE(b.isCorner(Point(0, 9)));
+		EXPECT_FALSE(b.isCorner(Point(9, 0)));
+		EXPECT_FALSE(b.isCorner(Point(9, 9)));
+	}
+
+	TEST(BoardTest, TestNotCorner) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_FALSE(b.isCorner(Point(1, 1)));
+		EXPECT_FALSE(b.isCorner(Point(0, 10)));
+		EXPECT_FALSE(b.isCorner(Point(5, 0)));
+	}
+
+	TEST(BoardTest, TestIsOccupied) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_FALSE(b.isOccupied(0));
+	}
+
+	TEST(BoardTest, TestIsWithinBoundary) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_TRUE(b.isWithinBoundary(Point(0, 0)));
+		EXPECT_TRUE(b.isWithinBoundary(Point(size-1, size-1)));
+		EXPECT_TRUE(b.isWithinBoundary(Point(0, size-1)));
+		EXPECT_TRUE(b.isWithinBoundary(Point(size-1, 0)));
+		EXPECT_TRUE(b.isWithinBoundary(Point(5, 0)));
+	}
+
+	TEST(BoardTest, TestNotIsWithinBoundary) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_FALSE(b.isWithinBoundary(Point(0, size)));
+		EXPECT_FALSE(b.isWithinBoundary(Point(size, size)));
+		EXPECT_FALSE(b.isWithinBoundary(Point(0, size)));
+		EXPECT_FALSE(b.isWithinBoundary(Point(size, 0)));
+		EXPECT_FALSE(b.isWithinBoundary(Point(-1, size+1)));
+	}
+
+	TEST(BoardTest, TestIsAvailable) {
+		int size{ 10 };
+		Board b(size);
+
+		EXPECT_TRUE(b.isAvailable(Point(0, 0)));
+		EXPECT_TRUE(b.isAvailable(Point(5, 0)));
+	}
+
+	TEST(BoardTest, TestNotIsAvailable) {
+		int size{ 10 };
+		Board b(size);
+
+		b.makeMove(Point(0, 0), Color::COLOR_BLUE);
+		EXPECT_FALSE(b.isAvailable(Point(0, 0)));
+		b.makeMove(Point(5, 0), Color::COLOR_RED);
+		EXPECT_FALSE(b.isAvailable(Point(5, 0)));
+	}
+} // namespace
+
--- hex/hex/hex.h
+++ hex/hex/hex.h
@@ -1,1 +1,67 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <chrono>
+
+// For the purpose of the assignment, definition and implementation
+// are in one file.
+
+//  Traditional Header definitions here
+static const int DefaultBoardSize{ 11 };
+enum class Color : std::int8_t { COLOR_BLANK = 0, COLOR_BLUE = 1, COLOR_RED = 2 };
+typedef std::tuple<int, int> Point;
+
+// Graph ADT
+class Graph
+{
+private:
+	int m_size;
+	std::vector<std::vector<int>> m_matrix;
+public:
+	Graph(int size);
+	void addEdge(int i, int j, int cost = 1) { m_matrix[i][j] = cost; }
+	void delEdge(int i, int j) { m_matrix[i][j] = 0; }
+
+};
+
+class Board
+{
+private:
+	int m_size;
+	std::vector<Color> m_squares;
+public:
+	Board(int size = DefaultBoardSize) : m_size{ size }
+	{
+		// We start with all the squares being unoccuppied.
+		m_squares.assign(m_size*m_size, Color::COLOR_BLANK);
+	}
+	int get_node(Point p) { return std::get<0>(p) * m_size + std::get<1>(p); }
+	Point get_square(int n) { return Point(n / m_size, n % m_size); }
+	bool isCorner(const Point& p);
+	bool isBlank(int n) { return m_squares[n] == Color::COLOR_BLANK; }
+	bool isOccupied(int n) { return !isBlank(n); }
+	bool isWithinBoundary(Point p);
+	bool isAvailable(Point p);
+	bool makeMove(Point p, Color c);
+};
+
+// Hex ADT
+// Hex is a game
+// Hex has a board
+// Hex has a players
+// Hex has a moves
+
+class Hex
+{
+private:
+	int m_size;
+	Graph m_graph{ m_size };
+	Board m_board{ m_size };
+public:
+	Hex(int size);
+};
+
+
+// End of hex header
